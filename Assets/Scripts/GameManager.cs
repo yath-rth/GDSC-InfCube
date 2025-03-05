d0d492aa1e8933a9a3051168c8c48ff9bb82f157
yath-rth <thecreator106@gmail.com>
2025-03-05 08:21:38 +0530

    Added main menu and pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,8 +10,8 @@ public class GameManager : MonoBehaviour
 
     public bool isGameOver = false;
 
-    [SerializeField] TMP_Text scoreText, endScreenScoreText;
-    [SerializeField] GameObject scoreText_obj, endScreen_Obj, deathParticles, otherUI_obj;
+    [SerializeField] TMP_Text scoreText, endScreenScoreText, pauseScreenScoreText;
+    [SerializeField] GameObject scoreText_obj, endScreen_Obj, deathParticles, otherUI_obj, pauseScreen_obj;
 
     int score;
 
@@ -38,9 +38,24 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isGameOver = true;
-        if(endScreen_Obj != null)endScreen_Obj.SetActive(true);
-        if(deathParticles != null) deathParticles.SetActive(true);
-        if(endScreenScoreText != null) endScreenScoreText.text = score.ToString("D5");
-        if(otherUI_obj != null) otherUI_obj.SetActive(false);
+        if (endScreen_Obj != null) endScreen_Obj.SetActive(true);
+        if (deathParticles != null) deathParticles.SetActive(true);
+        if (endScreenScoreText != null) endScreenScoreText.text = score.ToString("D5");
+        if (otherUI_obj != null) otherUI_obj.SetActive(false);
+    }
+
+    public void pause()
+    {
+        pauseScreenScoreText.text = score.ToString("D5");
+        otherUI_obj.SetActive(false);
+        pauseScreen_obj.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void resume()
+    {
+        otherUI_obj.SetActive(true);
+        pauseScreen_obj.SetActive(false);
+        Time.timeScale = 1;
     }
 }
